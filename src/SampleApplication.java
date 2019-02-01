95b270785d00ac8b3e6c96b11dfe6dc60cd641fe
gopac25 <39807096+gopac25@users.noreply.github.com>
2019-02-01 18:25:29 +0530

    Update SampleApplication.java

--- src/SampleApplication.java
+++ src/SampleApplication.java
@@ -1,14 +1,5 @@
-package com.codecentric.sample;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-
-
-@SpringBootApplication
-@ComponentScan("de.codecentric.sample")
-public class SampleApplication {
-    public static void main(String[] args) {
-            SpringApplication.run(SampleApplication.class, args);
-        }
+public class HelloWorld {
+  public static void main(String[] args) {
+    System.out.println("Hello, World");
+  }
 }
